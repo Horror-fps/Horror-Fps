34b8d8fcc819113d4c37962c29f6e2c6a8942ef2
Mackenzie Wilson <macky900078@gmail.com>
2016-01-20 10:25:22 -0500

    testing

---
+++ HORROR_FPS/Source/HORROR_FPS/HORROR_FPSGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "HORROR_FPSGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORROR_FPS_API AHORROR_FPSGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
